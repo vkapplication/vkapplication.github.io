8ba5b63f2f7cf012a5c7f728e843da436a7ff6af
Fedor <yakubovich.fedor@yandex.by>
2016-12-04 14:49:28 +0300

    1

--- getmembersvkapi.js
+++ getmembersvkapi.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
     // получаем информацию о группе и её участников
     function getMembers(group_id) {
-      VK.Api.call('groups.getById', {group_id: group_id, fields: 'photo_50,members_count', v: '5.6'}, function(r) {
+      VK.Api.call('groups.getById', {group_id: group_id, fields: 'photo_50,members_count', v: '5.27'}, function(r) {
           if(r.response) {
             $('.group_info')
             .html('<img src="' + r.response[0].photo_50 + '"/><br/>'
@@ -21,19 +21,18 @@ $(document).ready(function() {
 
     // получаем участников группы, members_count - количество участников
     function getMembers20k(group_id, members_count) {
-      var code =  'var members = API.groups.getMembers({"group_id": ' + group_id + ', "v": "5.6", "sort": "id_asc", "count": "1000", "offset": ' + membersGroups.length + '}).items;' // делаем первый запрос и создаем массив
+      var code =  'var members = API.groups.getMembers({"group_id": ' + group_id + ', "v": "5.27", "sort": "id_asc", "count": "1000", "offset": ' + membersGroups.length + '}).items;' // делаем первый запрос и создаем массив
           + 'var offset = 1000;' // это сдвиг по участникам группы
           + 'while (offset < 25000 && (offset + ' + membersGroups.length + ') < ' + members_count + ')' // пока не получили 20000 и не прошлись по всем участникам
           + '{'
-            + 'members = members + "," + API.groups.getMembers({"group_id": ' + group_id + ', "v": "5.6", "sort": "id_asc", "count": "1000", "offset": (' + membersGroups.length + ' + offset)}).items;' // сдвиг участников на offset + мощность массива
+            + 'members = members + "," + API.groups.getMembers({"group_id": ' + group_id + ', "v": "5.27", "sort": "id_asc", "count": "1000", "offset": (' + membersGroups.length + ' + offset)}).items;' // сдвиг участников на offset + мощность массива
             + 'offset = offset + 1000;' // увеличиваем сдвиг на 1000
           + '};'
           + 'return members;'; // вернуть массив members
 
       VK.Api.call("execute", {code: code}, function(data) {
         if (data.response) {
-          console.log(JSON.parse(data.response));
-          membersGroups = membersGroups.concat("[" + data.response + "]"); // запишем это в массив
+          membersGroups = membersGroups.concat(JSON.parse("[" + data.response + "]")); // запишем это в массив
           $('.member_ids').html('Загрузка: ' + membersGroups.length + '/' + members_count);
           if (members_count >  membersGroups.length) // если еще не всех участников получили
             setTimeout(function() { getMembers20k(group_id, members_count); }, 333); // задержка 0.333 с. после чего запустим еще раз
